1ef4b6a085fdbb0811943423b3225ebc98d0c9ae
AnandhikaS <anandhikasaravanan@gmail.com>
2024-07-20 16:17:15 +0530

    error fixes

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer,  Title,  SubTitle, ResumeButton } from './HeroStyle'
+import HeroImg from '../../images/HeroImage.jpg'
+
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src={HeroImg} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection;
---
+++ src/data/constants.js
@@ -0,0 +1,121 @@
+export const Bio = {
+    name: "Anandhika S",
+    
+    description:
+      "To leverage my technical skills and knowledge gained through education to contribute effectively to the dynamic environment",
+    github: "https://github.com/AnandhikaS",
+    resume:
+      "https://drive.google.com/file/d/1-0Xok6KP9bLHHQ-e-j99y-1oW8cjvDOh/view?pli=1",
+    linkedin: "https://www.linkedin.com/in/anandhika-s",
+    
+  };
+  
+  export const skills = [
+    {
+      title: "Skills",
+      skills: [
+        {
+          name: "React Native",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+       
+        
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        
+        
+      ],
+    },
+    
+  ];
+  
+  export const experiences = [
+  
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/C560BAQHHBElPR3DSOA/company-logo_200_200/0/1676655486977?e=2147483647&v=beta&t=TdgMM5dR2Uf1ZDoU9nC5CsaTCaQd0mo48FrMddhQxAo",
+      company: "AcmeGrade",
+      role: "Data Science",
+      date: "2022",
+      desc: "This provides the knowledge about datasets and visualization.",
+      
+      doc: "https://drive.google.com/file/d/1pFyk6ugVCyUQGAaHuWscQOACNq2z2VK-/view?pli=1",
+    },
+    {
+      id: 1,
+      img: "https://i.tracxn.com/logo/company/27_b7bd5c47-6382-4859-8b99-73da775ac3d9.PNG?height=120&width=120",
+      role: "Web Development & Server Hosting",
+      company: "Futurik Technologies Pvt,Ltd",
+      date: "2023",
+      desc: "I gained practical experience in designing and deploying responsive websites.",
+      doc:"https://drive.google.com/file/d/1pFidjoJXAxNw0VFlKdGgb3SXBtDzGEgc/view?pli=1",
+    },
+    {
+      id: 2,
+      img: "https://media.licdn.com/dms/image/C4E0BAQGs7EmZ0yQMpQ/company-logo_200_200/0/1661493099915?e=2147483647&v=beta&t=1UHBrhp9Fy6BawgDK56sdB-cXUS1BzYLcOIy32nZBEY",
+      role: "Artificial Intelligence and Machine Learning",
+      company: "InternPe",
+      date: "2024",
+      desc: "Understanding in AI/ML concepts",
+      doc: "https://drive.google.com/file/d/1NCOVSnUOLfTIsLaM6-GMVXkzCh_C4_v6/view?pli=1",
+    },
+   
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      
+      school: "Velammal College of Engineering and Technology, Madurai",
+      date: "2021 - 2025",
+      grade: "8.82 CGPA",
+      degree: "Bachelor of Technology - Information Technology with Creative Media (Honors)",
+    },
+    {
+      id: 1,
+      school: "Krishna International School (CBSE)",
+      date: "2019",
+      grade: "78.8%",
+      degree: "High School",
+    },
+    {
+      id: 2,
+      school: "Amrita Vidyalayam (CBSE)",
+      date: "2017 ",
+      grade: "75%",
+      degree: "Secondary School",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Excess Food Distribution App (FEED FORWARD)",
+      description:"Developed an Android application in Java to facilitate the donation of excess food, connecting donors with nearby recipients. The app features real-time location tracking, user authentication, and a streamlined donation process. It aims to reduce food waste and support community welfare efficiently", 
+      
+    },
+    {
+      id: 0,
+      title: "Agriculture Yield App (FIELDX)",
+      description:"Developed using React Native, offers farmers a robust tool for monitoring and enhancing crop productivity.",
+     
+    },
+   
+  ];
+  
+  
